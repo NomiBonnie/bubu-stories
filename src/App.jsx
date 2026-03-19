9fdc5ec2a9f8faefd6a77a20f9e8ab228a773773
Sam Yuan <samyuan@192.168.1.55>
2026-03-19 22:13:24 +0800

    feat: story 2 rainbow bridge, rounded covers, two-story grid

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import StoryReader from './StoryReader'
 import Home from './Home'
 import storiesIndex from '../stories/index.json'
-import storyData2026 from '../stories/2026-03-19.json'
+import story0319 from '../stories/2026-03-19.json'
+import story0320 from '../stories/2026-03-20.json'
 
 const storyMap = {
-  '2026-03-19': storyData2026
+  '2026-03-19': story0319,
+  '2026-03-20': story0320
 }
 
 export default function App() {
