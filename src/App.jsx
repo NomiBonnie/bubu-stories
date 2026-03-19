9a107d27d7c192c5104d10bc0187a23444292390
Sam Yuan <samyuan@192.168.1.55>
2026-03-19 23:04:04 +0800

    feat: stories 3+4 hedgehog friend + say sorry, with Sam+Tina

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import Home from './Home'
 import storiesIndex from '../stories/index.json'
 import story0319 from '../stories/2026-03-19.json'
 import story0320 from '../stories/2026-03-20.json'
+import story0321 from '../stories/2026-03-21.json'
+import story0322 from '../stories/2026-03-22.json'
 
 const storyMap = {
   '2026-03-19': story0319,
-  '2026-03-20': story0320
+  '2026-03-20': story0320,
+  '2026-03-21': story0321,
+  '2026-03-22': story0322
 }
 
 function getRoute() {
